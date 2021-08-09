8088d9526006e7a440b3e3d9fd49ca889e45f80d
sh15h4nk <shiva.shvvs@gmail.com>
2021-08-10 03:05:47 +0530

    location picker testing

--- src/Location.tsx
+++ src/Location.tsx
@@ -62,55 +62,85 @@
 
 
 
-import * as React from 'react';
-import { Marker, Popup } from 'react-leaflet';
-import { useState, useRef, useMemo, useCallback } from 'react';
+// import * as React from 'react';
+// import { Marker, Popup } from 'react-leaflet';
+// import { useState, useRef, useMemo, useCallback } from 'react';
 
-const center = {
-  lat: 51.505,
-  lng: -0.09,
-}
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.js';
+// import L from 'leaflet';
 
-export function Location() {
-  const [draggable, setDraggable] = useState(false)
-  const [position, setPosition] = useState(center)
-  const markerRef = useRef(null)
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker: any = markerRef.current;
-        if (marker != null) {
-          setPosition(marker.getLatLng())
-        }
-      },
-    }),
-    [],
-  )
-  const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d)
-  }, [])
+// // delete L.Icon.Default._getIconUrl;
 
-  return (
-    <Marker
-      draggable={draggable}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}>
-      <Popup minWidth={90}>
-        <span onClick={toggleDraggable}>
-          {draggable
-            ? 'Marker is draggable'
-            : 'Click here to make marker draggable'}
-        </span>
-      </Popup>
-    </Marker>
-  )
-}
+// L.Icon.Default.mergeOptions({
+//     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//     iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+// });
 
 
+// const center = {
+//   lat: 51.505,
+//   lng: -0.09,
+// }
 
 
+// export function Location() {
+//   const [draggable, setDraggable] = useState(false)
+//   const [position, setPosition] = useState(center)
+//   const markerRef = useRef(null)
+//   const eventHandlers = useMemo(
+//     () => ({
+//       dragend() {
+//         const marker: any = markerRef.current;
+//         if (marker != null) {
+//           setPosition(marker.getLatLng())
+//         }
+//       },
+//     }),
+//     [],
+//   )
+//   const toggleDraggable = useCallback(() => {
+//     setDraggable((d) => !d)
+//   }, [])
 
+//   return (
+//     <Marker
+//       // draggable={draggable}
+//       draggable={true}
+//       eventHandlers={eventHandlers}
+//       position={position}
+//       ref={markerRef}>
+//       <Popup minWidth={90}>
+//         <span onClick={toggleDraggable}>
+//           {draggable
+//             ? 'Marker is draggable'
+//             : 'Click here to make marker draggable'}
+//         </span>
+//       </Popup>
+//     </Marker>
+//   )
+// }
 
+import React from "react";
+import LocationPicker from "react-leaflet-location-picker";
 
+const Location = () => {
+  const pointVals = [50, 2];
+  const pointMode = {
+    banner: true,
+    control: {
+      values: pointVals,
+      onClick: (point: any) =>
+        console.log("I've just been clicked on the map!", point),
+      onRemove: (point: any) =>
+        console.log("I've just been clicked for removal :(", point)
+    }
+  };
+  const circleMode = {
+    banner: false
+  };
+  return <LocationPicker circleMode={circleMode} />;
+};
 
+export default Location;
--- src/LocationControl.tsx
+++ src/LocationControl.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { withJsonFormsControlProps } from "@jsonforms/react";
 import { MapContainer, TileLayer } from 'react-leaflet';
-import { Location } from './Location';
+import Location  from './Location';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -16,13 +16,8 @@ const center = {
 }
 
 const LocationControl = ({ data, handleChange, path }: LocationControlProps) => (
-	<MapContainer center={center} zoom={13} scrollWheelZoom={false} style={{ height: '100vh', width: '100wh' }}>
-	    <TileLayer
-	      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-	    />
+	    
 	    <Location />
-  	</MapContainer>
 );
 
 export default withJsonFormsControlProps(LocationControl);
