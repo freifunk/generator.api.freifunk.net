bbcbdfa15c8c94ecb047697c75454732eb49322f
sh15h4nk <shiva.shvvs@gmail.com>
2021-08-14 22:33:07 +0530

    loading api file data to marker

--- src/Location.tsx
+++ src/Location.tsx
@@ -1,146 +1,54 @@
-// import LocationPicker from '@react-location-picker';
+import * as React from 'react';
+import { Marker } from 'react-leaflet';
+import { useState, useRef, useMemo } from 'react';
 
-// interface LocationProps {
-// 	id?: string;
-// 	value: Object;
-// 	updateValue: (newValue: Object) => void;
-// }
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.js';
 
-// const defaultPosition = {
-//   lat: 27.9878,
-//   lng: 86.9250
-// };
-
-// export class Location extends Component {
-//   constructor (props) {
-//     super(props);
- 
-//     this.state = {
-//       address: "Kala Pattar Ascent Trail, Khumjung 56000, Nepal",
-//       position: {
-//          lat: 0,
-//          lng: 0
-//       }
-//     };
- 
-//     // Bind
-//     this.handleLocationChange = this.handleLocationChange.bind(this);
-//   }
- 
-//   handleLocationChange ({ position, address, places }) {
- 
-//     // Set new location
-//     this.setState({ position, address });
-//   }
- 
-//   render () {
-//     return (
-//       <div>
-//         <h1>{this.state.address}</h1>
-//         <div>
-//           <LocationPicker
-//             containerElement={ <div style={ {height: '100%'} } /> }
-//             mapElement={ <div style={ {height: '400px'} } /> }
-//             defaultPosition={defaultPosition}
-//             onChange={this.handleLocationChange}
-//           />
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+import L from 'leaflet';
 
 
+L.Icon.Default.imagePath='leaflet_images/';
 
+interface LocationProps {
+  id?: string;
+  value: {lat: number, lon: number};
+  updateCords: (newValue: Object) => void;
+}
 
+let center = {
+  lat: 51,
+  lng: 8
+}
 
+export const Location: React.FC<LocationProps> = ({ id, value, updateCords }) => {
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker: any = markerRef.current;
+        if (marker != null) {
+          var cords = marker.getLatLng();
+          setPosition(cords);
+          // console.log("The marker", cords);
+          updateCords({lat: cords.lat, lon: cords.lng});
+        }
+      },
+    }),
+    [updateCords],
+  )
+  if (value && !(value.lat === position.lat && value.lon === position.lng)){
+    const t = {lat: value.lat, lng: value.lon};
+    setPosition(t);
+  }
 
-
-
-
-
-
-
-
-// import * as React from 'react';
-// import { Marker, Popup } from 'react-leaflet';
-// import { useState, useRef, useMemo, useCallback } from 'react';
-
-// import 'leaflet/dist/leaflet.css';
-// import 'leaflet/dist/leaflet.js';
-// import L from 'leaflet';
-
-// // delete L.Icon.Default._getIconUrl;
-
-// L.Icon.Default.mergeOptions({
-//     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-//     iconUrl: require('leaflet/dist/images/marker-icon.png'),
-//     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-// });
-
-
-// const center = {
-//   lat: 51.505,
-//   lng: -0.09,
-// }
-
-
-// export function Location() {
-//   const [draggable, setDraggable] = useState(false)
-//   const [position, setPosition] = useState(center)
-//   const markerRef = useRef(null)
-//   const eventHandlers = useMemo(
-//     () => ({
-//       dragend() {
-//         const marker: any = markerRef.current;
-//         if (marker != null) {
-//           setPosition(marker.getLatLng())
-//         }
-//       },
-//     }),
-//     [],
-//   )
-//   const toggleDraggable = useCallback(() => {
-//     setDraggable((d) => !d)
-//   }, [])
-
-//   return (
-//     <Marker
-//       // draggable={draggable}
-//       draggable={true}
-//       eventHandlers={eventHandlers}
-//       position={position}
-//       ref={markerRef}>
-//       <Popup minWidth={90}>
-//         <span onClick={toggleDraggable}>
-//           {draggable
-//             ? 'Marker is draggable'
-//             : 'Click here to make marker draggable'}
-//         </span>
-//       </Popup>
-//     </Marker>
-//   )
-// }
-
-import React from "react";
-import LocationPicker from "react-leaflet-location-picker";
-
-const Location = () => {
-  const pointVals = [50, 2];
-  const pointMode = {
-    banner: true,
-    control: {
-      values: pointVals,
-      onClick: (point: any) =>
-        console.log("I've just been clicked on the map!", point),
-      onRemove: (point: any) =>
-        console.log("I've just been clicked for removal :(", point)
-    }
-  };
-  const circleMode = {
-    banner: false
-  };
-  return <LocationPicker circleMode={circleMode} />;
-};
-
-export default Location;
+  return (
+    <Marker
+    draggable={true}
+    eventHandlers={eventHandlers}
+    position={position}
+    ref={markerRef}>
+    </Marker>
+  )
+}
