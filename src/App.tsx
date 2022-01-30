198f1acd6204ae6f032864f576f6b0206c2af15a
Andreas Bräu <ab@andi95.de>
2022-01-30 18:17:28 +0100

    remove logo, too

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { JsonForms } from '@jsonforms/react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import logo from './logo.svg';
 import './css/App.css';
 import uischema from './uischema.json';
 import {
