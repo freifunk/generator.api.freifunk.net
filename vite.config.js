f7ea5c4d7bc6ac828f9c7e2d00bbe7cda21fe96f
Andreas Bräu <ab@andi95.de>
2025-03-09 23:36:22 +0100

    use path prefix
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/generator.api.freifunk.net/',
   plugins: [react()],
   resolve: {
     alias: {
