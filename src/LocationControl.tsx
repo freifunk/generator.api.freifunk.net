2b61392eff921f67da8634a132dce1ea34639d47
Andreas Bräu <ab@andi95.de>
2025-03-09 22:59:42 +0100

    center map on load
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- src/LocationControl.tsx
+++ src/LocationControl.tsx
@@ -1,8 +1,19 @@
 import { withJsonFormsControlProps } from "@jsonforms/react";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import { Location }  from './Location';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
+
+// MapCenter component to update the map view when center changes
+function MapUpdater({ center }: { center: { lat: number; lng: number } }) {
+  const map = useMap();
+  useEffect(() => {
+    if (center) {
+      map.setView([center.lat, center.lng], map.getZoom());
+    }
+  }, [map, center]);
+  return null;
+}
 
 interface LocationControlProps {
 	data: any;
@@ -18,13 +29,19 @@ let center = {
 const LocationControl: React.FC<LocationControlProps> = ({ data, handleChange, path }) => {
   const [mapCenter, setMapcenter] = useState(center);
 
-  if(data && !(mapCenter.lat === data.lat && mapCenter.lng === data.lon)){
-    const t = {lat: data.lat, lng: data.lon }
-    setMapcenter(t);
-  }
+  // Update map center when data changes
+  useEffect(() => {
+    if (data && typeof data.lat === 'number' && typeof data.lon === 'number') {
+      setMapcenter({
+        lat: data.lat,
+        lng: data.lon
+      });
+    }
+  }, [data]);
 
   return(
   	<MapContainer center={mapCenter} zoom={7} scrollWheelZoom={false} style={{ height: '20rem', width: '100wh' }}>
+      <MapUpdater center={mapCenter} />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
